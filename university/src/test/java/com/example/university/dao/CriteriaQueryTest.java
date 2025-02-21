a8577f6301677d4f5a63404612ac6335d0290ef1
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-02-21 14:52:41 +0000

    Query by Example

--- university/src/test/java/com/example/university/dao/CriteriaQueryTest.java
+++ university/src/test/java/com/example/university/dao/CriteriaQueryTest.java
@@ -7,14 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
 
 import com.example.university.business.CourseFilter;
 import com.example.university.business.DynamicQueryService;
 import com.example.university.business.UniversityService;
 import com.example.university.domain.Department;
+import com.example.university.domain.Person;
 import com.example.university.domain.Staff;
 import com.example.university.repo.DepartmentRepo;
 import com.example.university.repo.StaffRepo;
+
+import static org.springframework.data.domain.ExampleMatcher.matching;
 
 /**
  * Test Criteria-based queries
@@ -34,8 +38,14 @@ public class CriteriaQueryTest {
     @Test
     void findByCriteria() {
         UniversityFactory.fillUniversity(universityService);
-        Department humanities = departmentRepo.findByName("Humanities").get();
-        Staff professorBlack = staffRepo.findByLastName("Black")
+
+        Department humanities = departmentRepo.findOne(
+                Example.of(new Department("Humanities", null),
+                matching().withIgnoreCase())).get();
+
+        Staff professorBlack = staffRepo.findAll(
+                Example.of(new Staff(new Person(null, "Black")),
+                matching().withIgnoreCase()))
                 .stream().findFirst().get();
 
         System.out.println('\n' + "*** All Humanities Courses");
@@ -54,9 +64,9 @@ public class CriteriaQueryTest {
 
     private void find(CourseFilter filter) {
         queryService.filterByExample(filter)
-            .forEach(course -> {
-                assertTrue(filter.meetsCriteria(course));
-                System.out.println(course);
-            });
+                .forEach(course -> {
+                    assertTrue(filter.meetsCriteria(course));
+                    System.out.println(course);
+                });
     }
 }
