44d65a689a02d25eaaf02d2febbe7e7d59d4828e
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-02-21 15:11:45 +0000

    Spring Data REST

--- university/src/main/java/com/example/university/domain/ShowChair.java
+++ university/src/main/java/com/example/university/domain/ShowChair.java
@@ -1,10 +1,10 @@
 package com.example.university.domain;
 
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.data.rest.core.config.Projection;
-//
+import org.springframework.data.rest.core.config.Projection;
+
 // invoke from /departments/{id}?projection=showChair
-//@Projection(name = "showChair", types = { Department.class })
+@Projection(name = "showChair", types = { Department.class })
 public interface ShowChair {
   String getName();
 
--- university/src/main/java/com/example/university/repo/StaffRepo.java
+++ university/src/main/java/com/example/university/repo/StaffRepo.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.university.domain.Staff;
-//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-//
-//@RepositoryRestResource(path = "staff", collectionResourceRel = "staff")
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "staff", collectionResourceRel = "staff")
 public interface StaffRepo extends JpaRepository<Staff, Integer> {
 
   @Query("SELECT s FROM Staff s WHERE s.member.lastName = :lastName")
